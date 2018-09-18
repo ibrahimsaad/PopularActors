7dd729f8508a18de1cd4f6d7e344b2bc1af2cb00
Ibrahim Saad Mahmoud <ibrahimsaad20093@gmail.com>
2018-09-18 13:49:45 +0200

    create option menu item with search view

--- app/src/main/java/com/ibrahim/popularactors/MainActivity.java
+++ app/src/main/java/com/ibrahim/popularactors/MainActivity.java
@@ -1,13 +1,38 @@
 package com.ibrahim.popularactors;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    //<editor-fold desc="Option menu items creation and click">
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_popular_actors, menu);
+        MenuItem search = menu.findItem(R.id.action_search);
+        searchView = (SearchView) search.getActionView();
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    //</editor-fold>
 }
